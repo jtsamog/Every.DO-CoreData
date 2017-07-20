42b79fedb37a7e5aa57a86ba61b02276ad7e5771
Sam Ogunyowa <mailsamog@gmail.com>
2017-07-19 20:33:07 -0400

    Initial Commit

---
+++ Every.DO-CoreData/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Every.DO-CoreData
+//
+//  Created by Endeavour2 on 7/19/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Every.DO-CoreData/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Every.DO-CoreData
+//
+//  Created by Endeavour2 on 7/19/17.
+//  Copyright © 2017 SamOg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
